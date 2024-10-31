d5764fab5292baf7c59f3960f69fb059d566b788
jamullins <114003827+JaMullins@users.noreply.github.com>
2024-10-31 11:46:49 -0400

    Update next.bat

--- TkinterShennanigans - Copy/main/Very_Well_Hidden_Folder/next.bat
+++ TkinterShennanigans - Copy/main/Very_Well_Hidden_Folder/next.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-echo "C:\Users\%USERNAME%\AppData\Local\JetBrains\PyCharm Community Edition 2024.2.1\bin\pycharm64.exe" "C:\Users\%USERNAME%\PycharmProjects\TkinterShennanigans\main\progressed.py" > Hidden_File.bat
+echo "C:\Users\%USERNAME%\AppData\Local\JetBrains\PyCharm Community Edition 2024.2.1\bin\pycharm64.exe" "C:\Users\%USERNAME%\PycharmProjects\TkinterShennanigans\main\main.py" > Hidden_File.bat
 attrib +h Hidden_file.bat
 REM attrib -h %next.bat%
 exit
